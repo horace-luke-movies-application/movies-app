55f265c0de5e83693b3bb081cc3c00bc6d5a6b89
Horace Smith <horacesmith26@gmail.com>
2023-02-02 17:16:12 -0600

    inital files

--- js/index.js
+++ js/index.js
@@ -1,17 +1,8 @@
 
-// getMovies().then(movies => {
-//     movies.forEach(movie => {
-//         let movieContainer = $('<div></div>');
-//         let movieTitle = $('<h3></h3>').text(movie.title);
-//         let movieRuntime = $('<p></p>').text(`Runtime: ${movie.runtime}`)
-//         movieContainer.append(movieTitle);
-//         let movieRating = $('<p></p>').text(`Rating: ${movie.rating}`)
-//         let movieDirector = $('<p></p>').text(`Director: ${movie.director}`);
-//         let movieYear = $('<p></p>').text(`Year: ${movie.year}`);
-//         movieContainer.append(movieRuntime)
-//         movieContainer.append(movieRating)
-//         movieContainer.append(movieDirector)
-//         movieContainer.append(movieYear)
-//         $('body').append(movieContainer);
-//     });
-// });
+const items = document.querySelectorAll('.item');
+
+items.forEach(item => {
+    item.addEventListener('click', function() {
+        this.classList.toggle('flipped');
+    });
+});
