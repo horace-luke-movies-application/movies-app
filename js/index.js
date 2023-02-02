80fae2dbbac7faecc6285ce56579810f442402cd
Horace Smith <horacesmith26@gmail.com>
2023-02-02 15:36:50 -0600

    inital files

--- js/index.js
+++ js/index.js
@@ -1,7 +1,17 @@
-(async () => {
-    // This is the entry point for your application. Write all of your code here.
-    // Before you can use the database, you need to configure the "db" object 
-    // with your team name in the "js/movies-api.js" file.
-    
-    
-})();
+
+// getMovies().then(movies => {
+//     movies.forEach(movie => {
+//         let movieContainer = $('<div></div>');
+//         let movieTitle = $('<h3></h3>').text(movie.title);
+//         let movieRuntime = $('<p></p>').text(`Runtime: ${movie.runtime}`)
+//         movieContainer.append(movieTitle);
+//         let movieRating = $('<p></p>').text(`Rating: ${movie.rating}`)
+//         let movieDirector = $('<p></p>').text(`Director: ${movie.director}`);
+//         let movieYear = $('<p></p>').text(`Year: ${movie.year}`);
+//         movieContainer.append(movieRuntime)
+//         movieContainer.append(movieRating)
+//         movieContainer.append(movieDirector)
+//         movieContainer.append(movieYear)
+//         $('body').append(movieContainer);
+//     });
+// });
